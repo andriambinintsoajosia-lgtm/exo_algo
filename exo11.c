d2c16c11c3a7888853bd4f18d73425804843f5ab
josia <Andriambinintsoajosia@gmail.com>
2026-07-17 11:19:30 +0300

    Exercice 11 : Suite de Fibonacci recursive

---
+++ exo11.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int fibonacci(int n) {
+    if (n == 0) return 0;
+    if (n == 1) return 1;
+    return fibonacci(n - 1) + fibonacci(n - 2);
+}
+
+int main() {
+    int n = 8; // Suite : 0, 1, 1, 2, 3, 5, 8, 13, 21...
+    printf("Fibonacci(%d) = %d\n", n, fibonacci(n)); // Affiche 21
+    return 0;
+}
